b59790f02dc7302c2f9276bb2a5a50163bfb9466
FotoAleph <fotoaleph@gmail.com>
2022-11-24 06:49:26 -0500

    Primer_comit

---
+++ src/services/autenticacion.service.ts
@@ -0,0 +1,67 @@
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import { repository } from '@loopback/repository';
+import { Llaves } from '../config/llaves';
+import { Usuario } from '../models';
+import { UsuarioRepository } from '../repositories';
+const generador = require('password-generator');
+const cryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class AutenticacionService {
+  constructor(
+    @repository(UsuarioRepository)
+    public usuarioRepository:UsuarioRepository
+  ) {}
+
+  /*
+   * Add service methods here
+   */
+
+  GenerarClave() {
+    let clave = generador(8, false);
+    return clave;
+  }
+
+  CifrarClave(clave:string) {
+    let claveCrifada = cryptoJS.MD5(clave).toString();
+    return claveCrifada;
+  }
+  
+  IdentificarUsuario(usuario: string, clave:string){
+    try {
+      let p =this.usuarioRepository.findOne(
+        {where:{correo:usuario,contrasena:clave}}
+      );
+      if(p){
+        return p;
+      }
+      return false;
+
+    } catch {
+      return false;
+    }
+  }
+
+  GenerarTokenJWT(usuario: Usuario){
+    let token = jwt.sign({
+      data:{
+        id:usuario.id,
+        correo:usuario.correo,
+        nombre:usuario.nombre,
+        rol:usuario.rol
+      }
+
+    },
+    Llaves.claveJWT);
+    return token;
+  }
+  ValidarTokenJWT(token: string){
+    try{
+      let datos = jwt.verify(token,Llaves.claveJWT);
+      return datos;
+    }catch{
+      return false;
+    }
+  }
+}
